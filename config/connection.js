7171234ee5fbdb0e089f69e6755efb96b08fd31f
5820K <38088640+chuynh18@users.noreply.github.com>
2018-06-08 23:19:39 -0700

    modifications to make my app work on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 "use strict";
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "8U#mDA345vUk5W6vtjVCSMStLUWHmD!u",
     database: "burgers_db"
-});
+    });
+}
   
 connection.connect(function(err) {
     if (err) {
